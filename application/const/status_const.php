7f0f28758c8a61096875f507ec3090ff95af5f23
Ayuth Mangmesap <blackSource@windowslive.com>
2017-09-06 03:32:48 +0700

    finished with R
    read

--- application/const/status_const.php
+++ application/const/status_const.php
@@ -5,4 +5,10 @@ class Status {
 	const ERR_AUTHEN_INVALID = 300;
 	const ERR_AUTHEN_INVALID_MSG = 'Username หรือ Password ไม่ถูกต้อง';
 
+
+	# 4:: products 
+
+	const ERR_NO_PRODUCT_DATA = 400;
+	const ERR_NO_PRODUCT_DATA_MSG = 'ไม่พบข้อมูล Products ใน database';
+
 }
