55b3292032e1b99aedba6d13f22a7774362708c0
Ayuth Mangmesap <ayuth.mang@gmail.com>
2017-09-08 23:11:34 +0700

    add table ci_sessions for store sessions on db

--- database/os2_webapp.sql
+++ database/os2_webapp.sql
@@ -92,3 +92,11 @@ ALTER TABLE products ALTER COLUMN picture SET DEFAULT NULL ;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+        `id` varchar(128) NOT NULL,
+        `ip_address` varchar(45) NOT NULL,
+        `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+        `data` blob NOT NULL,
+        KEY `ci_sessions_timestamp` (`timestamp`)
+);
