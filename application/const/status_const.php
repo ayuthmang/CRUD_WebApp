9e638a7041b61b688e4d00500138fb67f55b0d90
Ayuth Mangmesap <blackSource@windowslive.com>
2017-09-09 01:16:55 +0700

    fix wording

--- application/const/status_const.php
+++ application/const/status_const.php
@@ -8,8 +8,8 @@ class Status {
 
 	# 4:: products 
 
-	const ERR_NO_PRODUCT_IN_DB = 400;
-	const ERR_NO_PRODUCT_IN_DB_MSG = 'ข้อมูล Products ใน database ว่างเปล่า';
+	const ERR_PRODUCT_NOT_FOUND = 400;
+	const ERR_PRODUCT_NOT_FOUND_MSG = 'ไม่พบ Product ในระบบ';
 	const ERR_PRODUCT_DUPLICATE = 401;
 	const ERR_PRODUCT_DUPLICATE_MSG = 'พบ Product ซ้ำในระบบ';
 
